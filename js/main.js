3af15bba5de33a247af67920ce961960296254c3
Mahdi-Rahmani <mah.rah79@yahoo.com>
2023-01-03 22:17:23 +0330

    updateUserInfo function is added to js

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@
         let userData = window.localStorage.getItem(username)
 
         if(userData == null){
-
+            userData = fetchData(baseURL+username)
         }else{
 
         }
@@ -35,41 +35,88 @@
         we have set some error message for some 400 family codes. if we have some problems
         in network connection we should set appropriate message.
     */
-        async function fetchData(url) {
-            try {
-                let response = await fetch(url)
-                if (!response.ok) {
+    async function fetchData(url) {
+        try {
+            let response = await fetch(url)
+            if (!response.ok) {
 
-                    if (response.status == 404)
-                        showError("User not found.")
+                if (response.status == 404)
+                    showError("User not found.")
         
-                    if (response.status == 403)
-                        showError("Forbidden (Maybe because of rate limit.)")
+                if (response.status == 403)
+                    showError("Forbidden (Maybe because of rate limit.)")
         
-                    showError(`Error in getting user info with error ${response.status}`)
-                }else{
-                    return response.json()
-                }
-            } catch(e) {
-                console.log(e)
-                showError("network error")
+                showError(`Error in getting user info with error ${response.status}`)
+            }else{
+                return response.json()
             }
-            
+        } catch(e) {
+            console.log(e)
+            showError("network error")
+        }   
+    }
+
+    // save user information in localstorage
+    function saveInfo(username, userData){
+        console.log("saving:"+userData)
+        window.localStorage.setItem(username, userData)
+    }
+    /*
+
+    */
+    function showError(text) {
+        errorWrapper.style.width = '100%'
+        errorWrapper.textContent = text
+        console.log('timeout staretd')
+        setTimeout(() => {
+            console.log('timeout finished')
+            errorWrapper.style.width = '0'
+            errorWrapper.textContent = ''
+        }, 4000)
+    }
+
+    /*
+        userData (maybe from local storage or from http get) can be updated 
+        from js. we update user info fields in html by this function.
+    */
+    function updateInfo(userData){
+
+        if(userData["avatar_url"] == "" || userData["avatar_url"] == null){
+            userImg.src = "img/notfound.png"
+        }else{
+            userImg.src = userData["avatar_url"]
         }
 
-        /*
-            
-        */
-        function showError(text) {
-            errorWrapper.style.width = '100%'
-            errorWrapper.textContent = text
-            console.log('timeout staretd')
-            setTimeout(() => {
-                console.log('timeout finished')
-                errorWrapper.style.width = '0'
-                errorWrapper.textContent = ''
-            }, 4000)
+        if(userData["blog"] == "" || userData["blog"] == null){
+            blog.innerText = "blog: Has not been set"
+        }else{
+            blog.innerText = userData["blog"]
         }
 
+        if(userData["name"] == "" || userData["name"] == null){
+            completeName.innerText = "name: Has not been set"
+        }else{
+            completeName.innerText = userData["name"]
+        }
+
+        if(userData["location"] == "" || userData["location"] == null){
+            userLocation.innerText = "location: Has not been set"
+        }else{
+            userLocation.innerText = userData["location"]
+        }
+
+        if(userData["bio"] == "" || userData["bio"] == null){
+            bio.innerText = "bio: Has not been set"
+        }else{
+            bio.innerText = userData["bio"]
+        }
+        
+        if(userData["max_lang"] == undefined){
+            reposUrl = userData["repos_url"]
+            return sortRepos(reposUrl)
+        }
+        else
+            setTopLanguage(userData["max_lang"])
+    }
 }
 )
