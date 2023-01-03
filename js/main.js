84aafc244d451bb3577eb89b8457d8aab65a9d8b
Mahdi-Rahmani <mah.rah79@yahoo.com>
2023-01-03 13:01:16 +0330

    javascript is started

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+(function (){
+    // we can set some variables for html elements that we need here
+    // elements in search box
+    let username = document.getElementById("username")
+    let submitButton = document.getElementById("submit")
+    //elements in information header
+    let userImg = document.getElementById("avatar")
+    let completeName = document.getElementById("complete-name")
+    let blog = document.getElementById("blog-addr")
+    let userLocation = document.getElementById("location")
+    //elements in information footer
+    let bio = document.getElementById("bio")
+    let progLang = document.getElementById("prog-lang")
+    
+    // github api for getting user info
+    const gitAPI = "https://api.github.com/users/"
+
+}
+)
